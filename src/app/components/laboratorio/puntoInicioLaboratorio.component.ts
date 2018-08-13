4701252e0a2807fd063d6fc379f64a0d768c6e0d
marcosantarelli <marcosavino19@gmail.com>
2018-08-13 14:31:37 -0300

    avances en filtros

--- src/app/components/laboratorio/puntoInicioLaboratorio.component.ts
+++ src/app/components/laboratorio/puntoInicioLaboratorio.component.ts
@@ -4,6 +4,9 @@ import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
 // import { Router } from '@angular/router';
 import { Auth } from '@andes/auth';
 import { Plex } from '@andes/plex';
+import * as enumerados from './../../utils/enumerados';
+import { OrganizacionService } from '../../services/organizacion.service';
+
 
 @Component({
     selector: 'gestor-protocolos',
@@ -13,24 +16,34 @@ import { Plex } from '@andes/plex';
 
 export class PuntoInicioLaboratorioComponent
 
- implements OnInit {
+    implements OnInit {
 
     public showListarProtocolos = true;
     public showProtocoloDetalle = false;
     public protocolos: any = [];
     public protocolo: any = {};
-    public fechaDesde: any;
-    public fechaHasta: any;
+    public fechaDesde: any = new Date();
+    public fechaHasta: any = new Date();
+    public prioridad: any;
     public parametros = [];
+    public origenEnum: any;
+    public prioridadesEnum;
+    public laboratorioInternoEnum: any;
 
-
-    constructor(public plex: Plex, private formBuilder: FormBuilder, 
+    constructor(public plex: Plex, private formBuilder: FormBuilder,
         public servicioPrestaciones: PrestacionesService,
-        public auth: Auth) { }
+        public auth: Auth,
+        private servicioOrganizacion: OrganizacionService,
+    ) { }
 
     ngOnInit() {
+        console.log(enumerados.getPrioridadesLab());
+
+        this.prioridadesEnum = enumerados.getPrioridadesLab();
+        this.origenEnum = enumerados.getOrigenLab();
+        this.laboratorioInternoEnum = enumerados.getLaboratorioInterno();
     }
-    
+
     // funciones
     refreshSelection(value, tipo) {
         this.parametros['tipoPrestacionSolicititud'] = '15220000';
@@ -53,9 +66,9 @@ export class PuntoInicioLaboratorioComponent
     };
 
     getNumeroProtocolo(registros) {
-        let registro : any = registros.find((reg) => {
+        let registro: any = registros.find((reg) => {
             console.log(reg);
-            return reg.nombre == "numeroProtocolo";
+            return reg.nombre === 'numeroProtocolo';
         });
         return registro.valor;
     }
@@ -63,13 +76,14 @@ export class PuntoInicioLaboratorioComponent
     getProtocolos(params: any) {
         this.servicioPrestaciones.get(params).subscribe(protocolos => {
             this.protocolos = protocolos;
+            console.log(this.protocolos);
         }, err => {
             if (err) {
                 console.log(err);
             }
         });
 
-        
+
     }
 
     estaSeleccionado(protocolo) {
@@ -78,38 +92,38 @@ export class PuntoInicioLaboratorioComponent
 
     verProtocolo(protocolo, multiple, e) {
         // Si se presionó el boton suspender, no se muestran otros protocolos hasta que se confirme o cancele la acción.
-        
-            if (protocolo && protocolo.id) {
-                // this.serviceAgenda.getById(agenda.id).subscribe(ag => {
-                    this.protocolo = protocolo;
-                    this.showListarProtocolos = false;
-                    this.showProtocoloDetalle = true;
+
+        if (protocolo && protocolo.id) {
+            // this.serviceAgenda.getById(agenda.id).subscribe(ag => {
+            this.protocolo = protocolo;
+            this.showListarProtocolos = false;
+            this.showProtocoloDetalle = true;
             // }
         }
     }
 
-    volverLista () {
+    volverLista() {
         this.protocolo = null;
         this.showListarProtocolos = true;
         this.showProtocoloDetalle = false;
     }
 
-    loadSectores (e) {
+    loadSectores(e) {
         return {};
     }
 
-    loadAreasValidacion (e) {
+    loadAreasValidacion(e) {
         return {};
     }
 
-    loadEfectores (e) {
+    loadEfectores(e) {
         return {};
     }
 
     loadProtocolo(e) {
         return [];
     }
-    
+
     loadSector(e) {
         return [];
     }
@@ -118,38 +132,57 @@ export class PuntoInicioLaboratorioComponent
         return [];
     }
 
+    loadOrganizacion(event) {
+        if (event.query) {
+            let query = {
+                nombre: event.query
+            };
+            this.servicioOrganizacion.get(query).subscribe(event.callback);
+        } else {
+            event.callback([]);
+        }
+
+
+    }
+
+    loadPrioridad(event) {
+        console.log(enumerados.getPrioridadesLab());
+        event.callback(enumerados.getPrioridadesLab());
+        return enumerados.getPrioridadesLab();
+    }
+
 
     // Hardcodeo
     protocols = [
         {
-        id: '716852',
-        fecha: '11/07/2018',
-        origen:'ambulatorio',
-        servicio:'clinica',
-        usuario:'lmonteverde',
-        solicitante:'wmolini',
-        fechaRegistro:'05/07/2018',
+            id: '716852',
+            fecha: '11/07/2018',
+            origen: 'ambulatorio',
+            servicio: 'clinica',
+            usuario: 'lmonteverde',
+            solicitante: 'wmolini',
+            fechaRegistro: '05/07/2018',
         },
         {
-        id: '516846',
-        fecha: '17/07/2018',
-        origen:'ambulatorio',
-        servicio:'clinica',
-        usuario:'lmonteverde',
-        solicitante:'wmolini',
-        fechaRegistro:'09/07/2018',
+            id: '516846',
+            fecha: '17/07/2018',
+            origen: 'ambulatorio',
+            servicio: 'clinica',
+            usuario: 'lmonteverde',
+            solicitante: 'wmolini',
+            fechaRegistro: '09/07/2018',
         },
         {
-        id: '354879',
-        fecha: '13/07/2018',
-        origen:'guardia',
-        servicio:'clinica',
-        usuario:'lmonteverde',
-        solicitante:'wmolini',
-        fechaRegistro:'11/07/2018',
+            id: '354879',
+            fecha: '13/07/2018',
+            origen: 'guardia',
+            servicio: 'clinica',
+            usuario: 'lmonteverde',
+            solicitante: 'wmolini',
+            fechaRegistro: '11/07/2018',
         }
     ];
-    
+
 }
 
 
